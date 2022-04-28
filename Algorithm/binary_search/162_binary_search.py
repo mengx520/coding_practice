0c41e3cf4c4d3da0fc08e9de763ffef2b4be2b6f
Alice Meng <mengx520@gmail.com>
2022-04-27 23:08:14 -0700

    Add solution and time complexity

---
+++ Algorithm/binary_search/162_binary_search.py
@@ -0,0 +1,15 @@
+'''
+0loglog(n)
+'''
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right :
+            middle = (left + right) // 2
+            if nums[middle] > nums[middle + 1]:
+                right = middle
+            else:
+                left = middle + 1
+        return left
+        
